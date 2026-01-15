5cb55316dc7c434f89b42294620bc9e337286c39
SeeleVollerei1810 <quann9076@gmail.com>
2026-01-15 14:55:18 +0700

    Update main.py

--- Heat-Stress Metrics/main.py
+++ Heat-Stress Metrics/main.py
@@ -4,7 +4,7 @@ from typing import Literal
 
 def main():
     OUTPUT_DIR = '/content/drive/MyDrive/Group Project 2025/results' #link_save
-    LAT_RANGE = (8.0, 24.0) # (currently Vietnam)
+    LAT_RANGE = (8.0, 24.0) #currently Vietnam
     LON_RANGE = (102.0, 110.0)
     NAN_METHOD: Literal['keep'] = 'keep'
 
